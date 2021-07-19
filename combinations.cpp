53a536b93171a3f4dfb89416a57654f3f6846802
miyagi-sensei <60123481+miyagi-sensei@users.noreply.github.com>
2021-07-19 14:50:18 +0800

    Add files via upload

---
+++ combinations.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int N, C;
+
+int main() {
+	cin >> N >> C;
+	// print all combinations of choosing C out of 0 ... N-1 (not necessarily in lexicographi order)
+	return 0;
+}
---
+++ permutations.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int N;
+
+int main() {
+	cin >> N;
+	// print all permutations of 0 ... N-1 (not necessarily in lexicographi order)
+	return 0;
+}
